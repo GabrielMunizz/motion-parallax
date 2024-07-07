dec13dc8a8a2455312bf14192ea8832e4999c2ed
Muniz <gabrielmfd@gmail.com>
2024-07-07 12:35:38 -0300

    Feat: cria arranjo de fotos no projeto e animação

---
+++ src/utils/photos.ts
@@ -0,0 +1,21 @@
+import photo1 from "../../public/85386503.jpg";
+import photo2 from "../../public/9eedf8719a3417d5af0bbaaff6f9345f.jpg";
+import photo3 from "../../public/Yehliu_promontory-HDR_Photo_HD_Wallpaper_medium.jpg";
+import photo4 from "../../public/cool-darth-vader-of-star-wars-tablet-r76jlsrtymubwzw9.jpg";
+import photo5 from "../../public/desktop-wallpaper-best-graphics-group-random-gaming.jpg";
+import photo6 from "../../public/jLsJPf.jpg";
+import photo7 from "../../public/rorschach-watchmen-wallpaper-preview.jpg";
+import photo8 from "../../public/wallpaper2you_418585.jpg";
+
+const photos = [
+  { url: photo1, width: "25vw", height: "25vh", top: "", left: "" },
+  { url: photo2, width: "38vw", height: "30vh", top: "-33vh", left: "-6vw" },
+  { url: photo3, width: "10vw", height: "28vh", top: "0vh", left: "-20vw" },
+  { url: photo4, width: "20vw", height: "55vh", top: "-20vh", left: "-38vw" },
+  { url: photo5, width: "15vw", height: "95vh", top: "", left: "40vw" },
+  { url: photo6, width: "28vw", height: "25vh", top: "30vh", left: "-35vw" },
+  { url: photo7, width: "15vw", height: "55vh", top: "-20vh", left: "58vh" },
+  { url: photo8, width: "45vw", height: "30vh", top: "30vh", left: "5vw" },
+];
+
+export default photos;
